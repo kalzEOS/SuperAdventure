4d73817d2911a63326715a29bbc99f69ea7b41e8
kalzEOS <49320606+kalzEOS@users.noreply.github.com>
2025-04-28 21:26:04 +0100

    more code

--- Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b49a25546a5843cf0321044abd0fd59c8f02c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
