13b49a25546a5843cf0321044abd0fd59c8f02c9
unknown <49320606+kalzEOS@users.noreply.github.com>
2025-04-28 20:57:07 +0100

    initial commit

---
+++ SuperAdventure/SuperAdventure.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SuperAdventure
+{
+    partial class SuperAdventure
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            lblHitPoints = new Label();
+            lblGold = new Label();
+            lblExperience = new Label();
+            lblLevel = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 25);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Hit points:";
+            label1.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Gold:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 125);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Experience:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 30);
+            label4.TabIndex = 3;
+            label4.Text = "Level:";
+            // 
+            // lblHitPoints
+            // 
+            lblHitPoints.AutoSize = true;
+            lblHitPoints.Location = new Point(220, 25);
+            lblHitPoints.Name = "lblHitPoints";
+            lblHitPoints.Size = new Size(0, 30);
+            lblHitPoints.TabIndex = 4;
+            // 
+            // lblGold
+            // 
+            lblGold.AutoSize = true;
+            lblGold.Location = new Point(220, 75);
+            lblGold.Name = "lblGold";
+            lblGold.Size = new Size(0, 30);
+            lblGold.TabIndex = 5;
+            // 
+            // lblExperience
+            // 
+            lblExperience.AutoSize = true;
+            lblExperience.Location = new Point(220, 125);
+            lblExperience.Name = "lblExperience";
+            lblExperience.Size = new Size(0, 30);
+            lblExperience.TabIndex = 6;
+            // 
+            // lblLevel
+            // 
+            lblLevel.AutoSize = true;
+            lblLevel.Location = new Point(220, 175);
+            lblLevel.Name = "lblLevel";
+            lblLevel.Size = new Size(0, 30);
+            lblLevel.TabIndex = 7;
+            // 
+            // SuperAdventure
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1517, 1381);
+            Controls.Add(lblLevel);
+            Controls.Add(lblExperience);
+            Controls.Add(lblGold);
+            Controls.Add(lblHitPoints);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = Color.Black;
+            Margin = new Padding(4);
+            Name = "SuperAdventure";
+            Text = "My Game";
+            Load += SuperAdventure_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lblHitPoints;
+        private Label lblGold;
+        private Label lblExperience;
+        private Label lblLevel;
+    }
+}
